68e3ae0bb541d2bdeec19f4b4af2cb0ebe52d096
yagna1612 <ypatel19@asu.edu>
2021-10-22 19:16:06 -0700

    Similarity Graph Done

--- Submission/Code/tasks/Task9.py
+++ Submission/Code/tasks/Task9.py
@@ -5,29 +5,22 @@ from numpy.random import randint
 from scipy.sparse.construct import random
 import pandas as pd
 if __name__ == "__main__":
-    subjects = [x for x in range(40)]
+    subjects = [x+1 for x in range(40)]
     arr = [[round((x+1)/randint(1,10),2) for x in range(40)]for y in range(40)]
     # print(arr)
     G = nx.DiGraph()
     for x in subjects:
         G.add_node(x)
-    val = pd.DataFrame(arr, columns = subjects)
-    print(val)
-    print('-----------')
-    for x in range(len(subjects)):
-        # val = (val.loc[x].sort_values(ascending=False))[:10]
-        df = val
-        val = pd.DataFrame(
-        data=df.columns.values[np.argsort(-df.values, axis=1)]
+    val = pd.DataFrame(arr, columns = subjects,index=subjects)
+    df = val
+    val = pd.DataFrame(
+        data=df.columns.values[np.argsort(-df.values, axis=1)],columns=subjects,index=subjects
     )
-        # print(val)
-        # print(len(val))
-        # print(len(arr[0]))
-        # break
-        G.add_edge(x,x+1,weight = arr[x][x])
+    for x in range(1,len(subjects)+1,1):
+        v = (val.loc[x])[:3]
+        for node in v:
+            G.add_edge(x,node,weight = arr[x-1][node-1])
 
-    print(G.nodes())
-    print(G.edges())
 
     # pos=nx.get_node_attributes(G,'pos')
     pos = nx.spring_layout(G,scale=2)
