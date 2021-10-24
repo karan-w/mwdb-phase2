f1ce5a4c52766ab955b74e29babfc769308dbc37
yagna1612 <ypatel19@asu.edu>
2021-10-24 00:49:53 -0700

    Task 9 Completed

--- Submission/Code/scripts/Task9_Parser.bat
+++ Submission/Code/scripts/Task9_Parser.bat
@@ -7,7 +7,8 @@ set model=HOG
 set image_type=cc
 set k_value=7
 set dimensionality_reduction_technique=LDA
+set sub_sub_matrix_path=D:\MWDB\mwdb-phase2\Submission\Outputs\Task4\output.json
 set images_folder_path=D:/MWDB/mwdb-phase2/Submission/Code/tasks/all
-set output_folder_path=D:/MWDB/mwdb-phase2/Submission/Outputs/Task1
-::python tasks/Task9_Parsers.py --n %n% --m %m% --input_subjects %input_subjects% --output_folder_path %output_folder_path%
-python tasks/Task1.py --model %model% --x %image_type% --k %k_value% --dimensionality_reduction_technique %dimensionality_reduction_technique% --images_folder_path %images_folder_path% --output_folder_path %output_folder_path%
+set output_folder_path=D:/MWDB/mwdb-phase2/Submission/Outputs/Task9
+python tasks/Task9.py --n %n% --m %m% --input_subjects %input_subjects% --output_folder_path %output_folder_path% --sub_sub_matrix_path %sub_sub_matrix_path%
+::python tasks/Task1.py --model %model% --x %image_type% --k %k_value% --dimensionality_reduction_technique %dimensionality_reduction_technique% --images_folder_path %images_folder_path% --output_folder_path %output_folder_path%
