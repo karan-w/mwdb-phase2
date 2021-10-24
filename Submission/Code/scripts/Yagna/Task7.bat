2dd0bb426ccc3c00646ab22d9d12d1087ff082e3
yagna1612 <ypatel19@asu.edu>
2021-10-24 14:53:07 -0700

    Bat Executables

---
+++ Submission/Code/scripts/Yagna/Task7.bat
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set query_image=D:/MWDB/mwdb-phase2/Submission/Code/tasks/all/image-cc-1-1.png
+set latent_semantics_file=D:\MWDB\mwdb-phase2\Submission\Outputs\Task1\2021-10-24_00-09-02\output.json
+set images_folder_path=D:/MWDB/mwdb-phase2/Submission/Code/tasks/all
+set output_folder_path=D:/MWDB/mwdb-phase2/Submission/Outputs/Task7
+python tasks/Task7.py --query_image %query_image% --latent_semantics_file %latent_semantics_file% --images_folder_path %images_folder_path% --output_folder_path %output_folder_path%
---
+++ Submission/Code/scripts/Yagna/Task9.bat
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set n=3
+set m=10
+set input_subjects=10,21,30
+set sub_sub_matrix_path=D:\MWDB\mwdb-phase2\Submission\Outputs\Task4\output.json
+set images_folder_path=D:/MWDB/mwdb-phase2/Submission/Code/tasks/all
+set output_folder_path=D:/MWDB/mwdb-phase2/Submission/Outputs/Task9
+python tasks/Task9.py --n %n% --m %m% --input_subjects %input_subjects% --output_folder_path %output_folder_path% --sub_sub_matrix_path %sub_sub_matrix_path%
